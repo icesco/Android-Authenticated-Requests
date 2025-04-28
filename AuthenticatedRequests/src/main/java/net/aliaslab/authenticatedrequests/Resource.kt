115d4cfe38749b1031fd31d8785e1ae28426bab3
Francesco Bianco <francesco.bianco@me.com>
2025-04-28 14:43:36 +0200

    More refinements

--- AuthenticatedRequests/src/main/java/net/aliaslab/authenticatedrequests/Resource.kt
+++ AuthenticatedRequests/src/main/java/net/aliaslab/authenticatedrequests/Resource.kt
@@ -1,9 +1,6 @@
 package net.aliaslab.authenticatedrequests
 
 import android.util.Log
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.withContext
 import kotlinx.serialization.json.Json
 import net.aliaslab.authenticatedrequests.authentication.AuthenticatedResource
 import net.aliaslab.authenticatedrequests.model.Result
@@ -22,56 +19,54 @@ interface Resource {
 
 class EmptyResult
 
-suspend inline fun <Input: URLTransformable, reified Output> Resource.request(parameter: Input, serializer: Json = Json { ignoreUnknownKeys = true }): Result<Output> {
+suspend inline fun <Input: URLTransformable, reified Output> Resource.request(
+    parameter: Input,
+    serializer: Json = Json { ignoreUnknownKeys = true }
+): Result<Output> {
 
     val request = this.urlRequest(parameter)
 
-    return coroutineScope {
-        withContext(Dispatchers.IO) {
-            try {
-                val authenticated = this@request as? AuthenticatedResource
-                if (authenticated != null) {
-                    val token = authenticated.authenticator().validToken()
-                    request.authentication = token.token_type + " " + token.access_token
-                }
-                val rawResult = HTTPClient.sendRequest(request)
+    return try {
+        val authenticated = this as? AuthenticatedResource
+        if (authenticated != null) {
+            val token = authenticated.authenticator().validToken()
+            request.authentication = token.token_type + " " + token.access_token
+        }
 
-                Log.d("Resource", "Raw result: $rawResult")
+        val rawResult = HTTPClient.sendRequest(request)
 
-                return@withContext when(Output::class.java) {
-                    String::class.java -> Result.Success(rawResult as Output)
-                    EmptyResult::class.java -> Result.Success(EmptyResult() as Output)
-                    else -> {
-                        val resultItem = serializer.decodeFromString<Output>(rawResult)
-                        Result.Success(resultItem)
-                    }
-                }
-            } catch (e: Exception) {
-                return@withContext Result.Error(e)
+        Log.d("Resource", "Raw result: $rawResult")
+
+        when (Output::class.java) {
+            String::class.java -> Result.Success(rawResult as Output)
+            EmptyResult::class.java -> Result.Success(EmptyResult() as Output)
+            else -> {
+                val resultItem = serializer.decodeFromString<Output>(rawResult)
+                Result.Success(resultItem)
             }
         }
+    } catch (e: Exception) {
+        Result.Error(e)
     }
 }
 
-suspend inline fun <Input: URLTransformable> Resource.download(parameter: Input, destinationFile: File): Result<Boolean> {
+suspend inline fun <Input: URLTransformable> Resource.download(
+    parameter: Input,
+    destinationFile: File
+): Result<Boolean> {
 
     val request = this.urlRequest(parameter)
 
-    return coroutineScope {
-        withContext(Dispatchers.IO) {
-            try {
-                val authenticated = this@download as? AuthenticatedResource
-                if (authenticated != null) {
-                    val token = authenticated.authenticator().validToken()
-                    request.authentication = token.token_type + " " + token.access_token
-                }
-
-                HTTPClient.sendDownloadRequest(request, destinationFile)
-
-                return@withContext Result.Success(true)
-            } catch (e: Exception) {
-                return@withContext Result.Error(e)
-            }
+    return try {
+        val authenticated = this as? AuthenticatedResource
+        if (authenticated != null) {
+            val token = authenticated.authenticator().validToken()
+            request.authentication = token.token_type + " " + token.access_token
         }
+
+        HTTPClient.sendDownloadRequest(request, destinationFile)
+        Result.Success(true)
+    } catch (e: Exception) {
+        Result.Error(e)
     }
 }
